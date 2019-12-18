1265d3cc29f33e2c686da7c60aa304be85308a52
Vladimir <vovan19977@gmail.com>
2019-12-18 21:58:52 +0300

    minor changes

--- src/FileSeeker/Config/Config.php
+++ src/FileSeeker/Config/Config.php
@@ -36,7 +36,7 @@ class Config
     private function readConfigFromFile()
     {
         $ds = DIRECTORY_SEPARATOR;
-        $path = dirname(__FILE__) . "{$ds}..{$ds}..{$ds}config";
+        $path = dirname(__FILE__) . "{$ds}..{$ds}..{$ds}..{$ds}config";
         $fileName = $path . $ds . 'config.yaml';
         $fileNameChunks = explode('.', $fileName);
         if (!file_exists($fileName) || array_pop($fileNameChunks) !== 'yaml')
